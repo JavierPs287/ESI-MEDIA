7cb36b415117dda7cc317b27b3a7125f02764f71
dbarchino <Daniel.Barchino@alu.uclm.es>
2025-10-11 16:56:01 +0200

    v1.0 diagrama clases

---
+++ be-esimedia/src/main/java/edu/uclm/esi/esimedia/be_esimedia/http/AudioController.java
@@ -0,0 +1,12 @@
+package edu.uclm.esi.esimedia.be_esimedia.http;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("circuits")
+@CrossOrigin("*")
+public class AudioController {
+    
+}
