9cfbf758c663a50e9ab831145a2fab9a62dc0d6f
JavierPs287 <your-email@example.com>
2025-10-17 14:06:26 +0200

    FE v1.3 Arreglo Visualizacion Fotos

--- fe-esimedia/src/app/app.component.ts
+++ fe-esimedia/src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-=======
 import { RegisteruserComponent } from './components/registeruser/registeruser.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RegisteruserComponent],
->>>>>>> a7b2ac1 (FE v1.2 RegistroUsuario)
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
