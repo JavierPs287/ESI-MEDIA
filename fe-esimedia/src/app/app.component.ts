4c84789d3053a477694e3831111599f00d635977
JavierPs287 <your-email@example.com>
2025-10-17 18:46:39 +0200

    FE v1.4 Login UI

--- fe-esimedia/src/app/app.component.ts
+++ fe-esimedia/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RegisteruserComponent } from './components/registeruser/registeruser.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisteruserComponent],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- fe-esimedia/src/app/app.routes.ts
+++ fe-esimedia/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginuserComponent } from './components/loginuser/loginuser.component';
+import { RegisteruserComponent } from './components/registeruser/registeruser.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginuserComponent },
+  { path: 'register', component: RegisteruserComponent }
+];
