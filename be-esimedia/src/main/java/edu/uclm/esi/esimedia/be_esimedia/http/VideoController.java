f03b1afbbb00c004788374500e9bf58a2759d647
dbarchino <Daniel.Barchino@alu.uclm.es>
2025-10-17 21:04:42 +0200

    Comienzo TDD en Alta de vídeo

---
+++ be-esimedia/src/main/java/edu/uclm/esi/esimedia/be_esimedia/http/VideoController.java
@@ -0,0 +1,7 @@
+package edu.uclm.esi.esimedia.be_esimedia.http;
+
+import edu.uclm.esi.esimedia.be_esimedia.services.VideoService;
+
+public class VideoController {
+    private final VideoService videoService;
+}
