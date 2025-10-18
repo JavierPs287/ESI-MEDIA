dfc835c27381261bd92ec074be662330a6166c9f
dbarchino <Daniel.Barchino@alu.uclm.es>
2025-10-18 21:17:42 +0200

    VideoController tests pasados y pom corregido

--- be-esimedia/src/main/java/edu/uclm/esi/esimedia/be_esimedia/model/Video.java
+++ be-esimedia/src/main/java/edu/uclm/esi/esimedia/be_esimedia/model/Video.java
@@ -2,7 +2,7 @@ package edu.uclm.esi.esimedia.be_esimedia.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "VIDEOS")
 public class Video extends Contenido {
     
 }
--- be-esimedia/src/main/java/edu/uclm/esi/esimedia/be_esimedia/services/VideoService.java
+++ be-esimedia/src/main/java/edu/uclm/esi/esimedia/be_esimedia/services/VideoService.java
@@ -1,5 +1,16 @@
 package edu.uclm.esi.esimedia.be_esimedia.services;
 
+import org.springframework.stereotype.Service;
+
+import edu.uclm.esi.esimedia.be_esimedia.dto.VideoDTO;
+
+@Service
 public class VideoService {
+
+    public String uploadVideo(VideoDTO videoDTO) {
+        // Lógica para subir el video (validaciones, almacenamiento, etc.)
+        // Por simplicidad, asumimos que el video se sube correctamente y devolvemos un ID simulado.
+        return "video12345";
+    }
 
 }
