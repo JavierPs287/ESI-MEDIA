3a05d405621ce646770830cecb1b8210c8f0079d
belennhr <belenn.hr@gmail.com>
2025-10-18 19:17:03 +0200

    FE ruta registerUser añadida

--- fe-esimedia/src/app/app.routes.ts
+++ fe-esimedia/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegisteruserComponent } from './components/registeruser/registeruser.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {
+        path: 'registerUser',
+        component: RegisteruserComponent
+    }
+
+];
