f56b851a2b224c28bcb64eb457a1b40423d94283
belennhr <belenn.hr@gmail.com>
2025-10-19 17:43:36 +0200

    FE v3.0 AllRegistros

--- fe-esimedia/src/app/components/navbar/navbar.component.ts
+++ fe-esimedia/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from "@angular/router";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private readonly router: Router) {}
 
+  get showNavbar(): boolean {
+    const url = this.router.url;
+    return !url.includes('/register/creator') && !url.includes('/register/admin');
+  }
 }
