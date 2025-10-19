a93aa94e1fbb284e7f0dfba27a3ed5c8c19b8bf6
JavierPs287 <your-email@example.com>
2025-10-19 17:18:06 +0200

    FE+BE v1.1 Arreglo mostrar mensajes de error del BE en el FE

--- fe-esimedia/src/app/app.component.ts
+++ fe-esimedia/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RegisteruserComponent } from './components/registeruser/registeruser.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisteruserComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
