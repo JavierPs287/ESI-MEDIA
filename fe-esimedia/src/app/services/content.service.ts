959b18783ce52f07bb1abce047b6cfd31832c3d1
JavierPs287 <your-email@example.com>
2025-11-17 22:59:18 +0100

    FIX FE contenidos audio

--- fe-esimedia/src/app/services/content.service.ts
+++ fe-esimedia/src/app/services/content.service.ts
@@ -103,11 +103,16 @@ export class ContentService {
    */
   getAudioByUrlId(urlId: string): Observable<Blob> {
     return new Observable<Blob>(observer => {
-      fetch(`http://localhost:8081/usuario/audio/${urlId}`, {
+      fetch(`${environment.apiUrl}/usuario/audio/${urlId}`, {
         method: 'GET',
         credentials: 'include',
       })
-      .then(response => response.blob())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.blob();
+      })
       .then(blob => {
         observer.next(blob);
         observer.complete();
